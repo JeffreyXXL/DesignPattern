022dbd976c5ac24d915671786661d9e754e9dff7
Jeffrey <xiajie727@vip.qq.com>
2025-08-10 18:41:46 +0800

    feat: 单例模式学习内容

---
+++ Singleton/Program.cs
@@ -0,0 +1,81 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1、普通调用
+            var instance1 = new Common();
+            var instance2 = new Common();
+            CheckInstance(instance1, instance2);
+
+            //2、懒汉式加载
+            var lazyInstance1 = Singleton_Lazy.GetInstance();
+            var lazyInstance2 = Singleton_Lazy.GetInstance();
+            CheckInstance(lazyInstance1, lazyInstance2);
+
+            //3、饿汉式加载
+            var eagerInstance1 = Singleton_Eager.Instance;
+            var eagerInstance2 = Singleton_Eager.Instance;
+            CheckInstance(eagerInstance1, eagerInstance2);
+
+            //4、Lazy<T>延时加载
+            var lazyTInstance1 = Singleton_LazyT.Instance;
+            var lazyTInstance2 = Singleton_LazyT.Instance;
+            CheckInstance(lazyTInstance1, lazyTInstance2);
+
+            //5、容器加载
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterType<Singleton_Container>().SingleInstance();
+            builder.RegisterType<Common_Container>();
+
+            //容器单例
+            IContainer container = builder.Build();
+            var containerInstance1 = container.Resolve<Singleton_Container>();
+            var containerInstance2 = container.Resolve<Singleton_Container>();
+            CheckInstance(containerInstance1, containerInstance2);
+
+            //容器普通
+            var containerCommonInstance1 = container.Resolve<Common_Container>();
+            var containerCommonInstance2 = container.Resolve<Common_Container>();
+            CheckInstance(containerCommonInstance1, containerCommonInstance2);
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 判断是否式同一个实例
+        /// </summary>
+        /// <param name="obj1"></param>
+        /// <param name="obj2"></param>
+        private static void CheckInstance(object obj1, object obj2)
+        {
+            Console.WriteLine("*****************************************************");
+            //检查是不是同一个实例
+            Console.WriteLine($"{obj1.GetType().Name}两个实例是否是相同实例：{object.ReferenceEquals(obj1, obj2)}");
+            //查看哈希地址
+            int hash1 = RuntimeHelpers.GetHashCode(obj1);
+            int hash2 = RuntimeHelpers.GetHashCode(obj2);
+            Console.WriteLine($"obj1 的哈希地址是{hash1}");
+            Console.WriteLine($"obj2 的哈希地址是{hash2}");
+            if (hash1 == hash2)
+            {
+                Console.WriteLine($"********** obj1 和 obj2 的哈希地址一致");
+            }
+            else
+            {
+                Console.WriteLine($"********** obj1 和 obj2 的哈希地址不一致");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// 懒汉式
+    /// </summary>
+    public class Singleton_Lazy
+    {
+        //1、隐藏构造函数
+        private Singleton_Lazy() { }
+
+        //2、建立instance变量
+        private static Singleton_Lazy _instance;
+
+        //5、定义对象用于锁
+        private static readonly object _lock = new object();
+
+        //3、构造获取实例的方法
+        public static Singleton_Lazy GetInstance()
+        {
+            //4、创建实例
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton_Lazy();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// 饿汉式
+    /// </summary>
+    public class Singleton_Eager
+    {
+        //1、隐藏构造函数
+        private Singleton_Eager() { }
+
+        //2、建立instance变量，同时直接赋值
+        private static readonly Singleton_Eager _instance = new Singleton_Eager();
+
+        //3、静态属性给出实例
+        public static Singleton_Eager Instance => _instance;
+    }
+
+    /// <summary>
+    /// Lazy加载
+    /// </summary>
+    public class Singleton_LazyT
+    {
+        //1、隐藏构造函数
+        private Singleton_LazyT() { }
+
+        //2、建立instance变量，同时使用Lazy使其延时加载
+        private static readonly Lazy<Singleton_LazyT> _lazy = new Lazy<Singleton_LazyT>(() => new Singleton_LazyT());
+
+        //静态属性给出实例
+        public static Singleton_LazyT Instance => _lazy.Value;
+    }
+
+    /// <summary>
+    /// 普通类
+    /// </summary>
+    public class Common
+    {
+
+    }
+
+    /// <summary>
+    /// 容器加载单例
+    /// </summary>
+    public class Singleton_Container
+    {
+
+    }
+
+    /// <summary>
+    /// 容器加载非单例
+    /// </summary>
+    public class Common_Container
+    {
+
+    }
+}
